18f1fb00da0ff570b16fc14e7fd4b350ae2b781f
archx3 <georgeranch31@gmail.com>
2020-02-07 16:49:22 +0000

    refactor (index): Manually add subcommands
    Manually add subcommands as opposed to creating them programmatically within a loop

--- bin/index.js
+++ bin/index.js
@@ -4,64 +4,22 @@ require('@babel/register');
 
 const subCommands = require('../lib/commands');// print menu
 
-//const app = require('commander');// import function to list coffeee menu
-//app.version('0.0.1');
-
 const commander = require('commander');
 const program = new commander.Command();
 program.version('0.0.1')
+  .usage('<command> [options]')
   .option('-v, --version', 'Show the version of the app')
   .option('-h, --help', 'Show help screen');
-//help();
 
-Object.keys(subCommands).forEach((subCommand) => {
-  //console.log(subCommands[subCommand]);
-  /*let requiredParams = '';
-  let optionalParams = '';
-  if (subCommands[subCommand].params) {
-    if (subCommands[subCommand].params.required) {
-      requiredParams = subCommands[subCommand].params.required
-        .reduce(function (acc, curr, i) {
-          return acc + ' <' + curr + '>';
-        }, ' ');
-    }
-    if (subCommands[subCommand].params.optional) {
-      optionalParams = subCommands[subCommand].params.optional
-        .reduce(function (acc, curr, i) {
-          return acc + ' [' + curr + ']';
-        }, ' ');
-    }
-  }*/
-  let command = program.command(subCommand) // sub-command name
-    .alias(subCommands[subCommand].alias)
-    .description(subCommands[subCommand].description);
+let mc =  subCommands["make-controller"];
 
-  if (subCommands[subCommand].params) {
-    if (subCommands[subCommand].params.required) {
-      subCommands[subCommand].params.required.forEach((param) => {
-        command.option(`${ param.alias ? '-' + param.alias + ',' : ''}--${param.name} <${param.name}>`, param.description || '');
-      })
-    }
-  }
-  command.action(function (name) {
-    subCommands[subCommand]['handler'](name)
+program.command("make-controller") // sub-command name
+  .description('Create a controller code stub') // command description
+  .alias("mc")
+  .option('-n,--name <name>', 'Name of the controller (lower caps)')
+  .action(function (name) {
+    mc.handler(name)
   });
-
-  /*if (subCommands[subCommand].description) {
-    command.description(subCommands[subCommand].description) // command description
-  }
-
-  if (subCommands[subCommand].alias) {
-    command.alias(subCommands[subCommand].alias) // alternative sub-command is `al`
-  }*/
-});
-
-//program.command("make-controller") // sub-command name
-//  .description('Create a controller code stub') // command description
-//  .alias("mc")
-//  .action(function () {
-//    subCommands["make-controller"].handler()
-//  });
 
 if (program.help) {
   subCommands.help.handler();
@@ -71,15 +29,15 @@ if (program.help) {
 // node's emit() is immediate
 
 program.on('--help', function(){
-  console.log('')
+  console.log('');
   console.log('Examples:');
-  console.log('  $ custom-help --help');
-  console.log('  $ custom-help -h');
+  console.log(' $ custom-help --help');
+  console.log(' $ custom-help -h');
   subCommands.help.handler();
 });
 
 let init = () => {
-
+  subCommands.help.handler();
 };
 
 // allow commander to parse `process.argv`
