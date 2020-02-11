1a9f78098ca70875451684203607b50bede4c108
archx3 <georgeranch31@gmail.com>
2020-02-11 06:16:06 +0000

    feat (index): Update root command and Add create-route sub commands.js
    Add alias to root command
    Add create-route sub command and it's properties

--- bin/index.js
+++ bin/index.js
@@ -7,6 +7,7 @@ const subCommands = require('../lib/commands');// print menu
 const commander = require('commander');
 const program = new commander.Command();
 program.version('0.0.1')
+  .alias("qo")
   .usage('<command> [options]')
   .option('-v, --version', 'Show the version of the app')
   .option('-h, --help', 'Show help screen');
@@ -14,11 +15,27 @@ program.version('0.0.1')
 let mc =  subCommands["make-controller"];
 
 program.command("make-controller") // sub-command name
-  .description('Create a controller code stub') // command description
+  .description('Create a directory for a new controller with associated boilerplate code') // command description
   .alias("mc")
-  .option('-n,--name <name>', 'Name of the controller (lower caps)')
+  .option('-n, --name <name>', 'Name of the controller (lower caps)')
   .action(function (name) {
     mc.handler(name)
+  });
+
+let cr = subCommands['create-route'];
+
+program.command("create-route") // sub-command name
+  .description('Create a route table file in /src/routes with code stubs') // command description
+  .alias("cr")
+  .option('-n, --name <name>', 'Name of the route table (lower caps)')
+  .action(function (name) {
+    cr.handler(name)
+  });
+
+program.command('list-routes')
+  .description('List all mounted routes when app is running')
+  .alias('ls')
+  .action(() => {
   });
 
 if (program.help) {
@@ -28,12 +45,16 @@ if (program.help) {
 // must be before .parse() since
 // node's emit() is immediate
 
+//program.help(function () {
+  //return subCommands.help.handler();
+//});
+
 program.on('--help', function(){
   console.log('');
   console.log('Examples:');
   console.log(' $ custom-help --help');
   console.log(' $ custom-help -h');
-  subCommands.help.handler();
+  // subCommands.help.handler();
 });
 
 let init = () => {
